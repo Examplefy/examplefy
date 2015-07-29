bd2f03b74d0c6e861873bbb81d2783fcb03bc9b1
Cal Peyser <calpeyser@gmail.com>
2015-07-29 18:41:00 -0400

    index changes

--- examplefy/admin.py
+++ examplefy/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from examplefy.models import *
 
-admin.site.register(Example);
-admin.site.register(Tag);
+admin.site.register(Example)
+admin.site.register(Topic)
+admin.site.register(Concept)
+admin.site.register(Answer)
--- examplefy/models.py
+++ examplefy/models.py
@@ -1,24 +1,43 @@
 from django.db import models
 
-class Tag(models.Model):
-    """
-    Singleton objects representing "tags" that can be associated with
-    an example.  An example can have many tags, and tags can be heirarchical.
+# class Tag(models.Model):
+#     """
+#     Singleton objects representing "tags" that can be associated with
+#     an example.  An example can have many tags, and tags can be heirarchical.
+#
+#     In general, tags should be created by superusers at the admin site.
+#     """
+#     name     = models.CharField(max_length=100)
+#     parent   = models.ForeignKey('self', null=True, blank=True)
+#
+#     def __unicode__(self):
+#         return unicode(self.name)
 
-    In general, tags should be created by superusers at the admin site.
+class Topic(models.Model):
     """
-    name     = models.CharField(max_length=100)
-    parent   = models.ForeignKey('self', null=True, blank=True)
+    Represents a high level topic.
+    """
+    name = models.CharField(max_length=50)
 
     def __unicode__(self):
         return unicode(self.name)
+
+class Concept(models.Model):
+    """
+    Represents a concept, which exists within a Topic
+    """
+    name = models.CharField(max_length=50)
+    topic = models.ForeignKey(Topic)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
 
 class Example(models.Model):
     """
     Represents an example.  The fundamental data type for the application.
     """
     title     = models.CharField(max_length=50)
-    tags      = models.ManyToManyField(Tag, blank=True)
     content   = models.TextField()
 
     def __unicode__(self):
@@ -26,3 +45,13 @@ class Example(models.Model):
 
     class Meta:
         ordering = ('title',)
+
+class Answer(models.Model):
+    """
+    Represents a solution to an example.
+    """
+    example = models.ForeignKey(Example)
+    content = models.TextField()
+
+    def __unicode__(self):
+        return unicode(self.title)
