2bf37935b7983d3ebe8f029dfce307bfc880ef00
Cal Peyser <calpeyser@gmail.com>
2015-08-09 14:59:09 -0400

    ask - javascript form

--- examplefy/views.py
+++ examplefy/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.views.generic import View
 from haystack.forms import SearchForm, ModelSearchForm
 from haystack.generic_views import SearchView
-from .models import Example, Topic
+from .models import Example, Topic, Concept
 from .forms import *
 
 def homepage(request):
@@ -29,7 +29,21 @@ class ExamplefySearchView(SearchView):
     form_class = SearchForm
 
 def ask_view(request):
+    data_for_form = {}
+    data_for_form["topics"] = Topic.objects.all()
+
+    return render(request, 'ask.html', {"data": data_for_form})
+
+    """
     form = TopicForm()
     entered_data = {}
     entered_data["topics"] = Topic.objects.all()
+    if not "topic" in request.GET:
+        entered_data["state"] = "topic"
+    elif not "concepts" in request.GET:
+        entered_data["topic"] = request.GET['topic']
+        entered_data["state"] = "concept"
+        print Concept.objects.all().filter(topic=Topic(name=entered_data["topic"]))
+        entered_data["concepts"] = Concept.objects.all().filter(topic=Topic(name=entered_data["topic"]))
     return render(request, 'ask.html', {"form": form, "entered_data": entered_data})
+    """
---
+++ project/static/js/example_form.js
@@ -0,0 +1,3 @@
+function say_hello(thing_to_say) {
+  alert(thing_to_say)
+}
