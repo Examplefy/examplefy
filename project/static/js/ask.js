408decde183c3e1b7d258e0619d36723ea8b5add
Cal Peyser <calpeyser@gmail.com>
2015-08-21 10:33:16 -1000

    logo

--- project/static/js/ask.js
+++ project/static/js/ask.js
@@ -9,5 +9,27 @@ function retrieve(key) {
 }
 
 $('li.topic_list_element').on('click', function(e) {
-    alert("hello!")
+  record("topic", this.id)
+
+  var topic_button = $('#topic_button')
+  topic_button.text("Topic: " + this.id)
+
+
+  var concept_dropdown = $('#concept_dropdown')
+  concept_dropdown.html("")
+  for (i = 0; i < window.DATA["concepts"][this.id].length; i++) {
+    var concept = window.DATA["concepts"][this.id][i]
+    concept_dropdown.append('<li class="concept_list_element" id="' + concept + '"><a>' + concept + '</a></li>')
+  }
+  bind_concept()
 })
+
+function bind_concept() {
+  var concept_button = $('#concept_button')
+  concept_button.text("Choose a Concept")
+
+  $('li.concept_list_element').on('click', function(e) {
+    record("concept", this.id)
+    concept_button.text("Concept: " + this.id)
+  })
+}
