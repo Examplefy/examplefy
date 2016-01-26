919246e9215bdf12601d7ab3e9834fa5c878a5be
Maelstroms38 <stroms38@gmail.com>
2016-01-25 23:17:00 -0500

    for the love of static files

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -1,3 +1,10 @@
+
+#static is here mvpland_static
+#postgresql -- mvpland
+#username -- cfedeploy
+#password -- ##
+
+
 """
 Django settings for ecommerce project.
 
@@ -11,184 +18,156 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-#root of project
+from django.conf import settings
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+if not settings.DEBUG:
+	import os
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'csqwlmc8s55o($rt6ozh7u+ui9zb-et00w$d90j8$^!nvj41_r'
+	BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+	#root of project
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+	# Quick-start development settings - unsuitable for production
+	# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-ALLOWED_HOSTS = ['*']
+	# SECURITY WARNING: keep the secret key used in production secret!
+	SECRET_KEY = 'csqwlmc8s55o($rt6ozh7u+ui9zb-et00w$d90j8$^!nvj41_r'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'examplefy@gmail.com'
-EMAIL_HOST_PASSWORD = 'yourpassword'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+	# SECURITY WARNING: don't run with debug turned on in production!
+	DEBUG = False
 
-''' 
-If using gmail, you will need to
-unlock Captcha to enable Django 
-to  send for you:
-https://accounts.google.com/displayunlockcaptcha
-'''
+	ADMINS = (
+		("Justin", "codingforentrepreneurs@gmail.com"),
 
+		)
 
+	ALLOWED_HOSTS = ['cfedeploy.webfactional.com', 'trydjango.com', 'www.trydjango.com']
+	#purchasing domain name http://name.com
 
-# Application definition
+	EMAIL_HOST = 'smtp.gmail.com'
+	EMAIL_HOST_USER = 'yourgmail@gmail.com'
+	EMAIL_HOST_PASSWORD = 'yourpassword'
+	EMAIL_PORT = 587
+	EMAIL_USE_TLS = True
 
-INSTALLED_APPS = (
-    #django app
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    #third party apps
-    'crispy_forms',
-    'registration',
-    #my apps
-    'answers',
-    'newsletter',
-    "products",
-    "carts",
-    "billing",
-    "django_filters"
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-)
-
-ROOT_URLCONF = 'ecommerce.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'ecommerce.wsgi.application'
+	''' 
+	If using gmail, you will need to
+	unlock Captcha to enable Django 
+	to  send for you:
+	https://accounts.google.com/displayunlockcaptcha
+	'''
 
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+	# Application definition
 
+	INSTALLED_APPS = (
+	    #django app
+	    'django.contrib.admin',
+	    'django.contrib.auth',
+	    'django.contrib.contenttypes',
+	    'django.contrib.sessions',
+	    'django.contrib.sites',
+	    'django.contrib.messages',
+	    'django.contrib.staticfiles',
+	    #third party apps
+	    'crispy_forms',
+	    'registration',
+	    #my apps
+	    'newsletter',
+	)
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
+	MIDDLEWARE_CLASSES = (
+	    'django.contrib.sessions.middleware.SessionMiddleware',
+	    'django.middleware.common.CommonMiddleware',
+	    'django.middleware.csrf.CsrfViewMiddleware',
+	    'django.contrib.auth.middleware.AuthenticationMiddleware',
+	    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+	    'django.contrib.messages.middleware.MessageMiddleware',
+	    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	    'django.middleware.security.SecurityMiddleware',
+	)
 
-LANGUAGE_CODE = 'en-us'
+	ROOT_URLCONF = 'ecommerce.urls'
 
-TIME_ZONE = 'EST'
+	TEMPLATES = [
+	    {
+	        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+	        'DIRS': [os.path.join(BASE_DIR, "templates")],
+	        'APP_DIRS': True,
+	        'OPTIONS': {
+	            'context_processors': [
+	                'django.template.context_processors.debug',
+	                'django.template.context_processors.request',
+	                'django.contrib.auth.context_processors.auth',
+	                'django.contrib.messages.context_processors.messages',
+	            ],
+	        },
+	    },
+	]
 
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-'''Image storage Amazon S3'''
-
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'examplefy'
-
-S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = S3_URL
-
-# #BOTO S3 Storage for Production ONLY
-# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static_root")
-
-MEDIA_URL = S3_URL
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media_root")
-
-# TEMPLATE_DIRS = (
-#     os.path.join(BASE_DIR, "templates"),
-# )
-# here() gives us file paths from the root of the system to the directory
-# holding the current file.
-here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
-
-PROJECT_ROOT = here("..")
-# root() gives us file paths from the root of the system to whatever
-# folder(s) we pass it starting at the parent directory of the current file.
-root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
-
-TEMPLATE_DIRS = (
-    root('templates'),
-)
-
-'''Static storage'''
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static', "our_static"),
-)
-
-MEDIA_URL = '/media/'
-
-PROTECTED_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "protected_root")
-
-#Crispy FORM TAGs SETTINGS
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+	WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 
-#DJANGO REGISTRATION REDUX SETTINGS
-ACCOUNT_ACTIVATION_DAYS = 7
-REGISTRATION_AUTO_LOGIN = True
-SITE_ID = 1
-LOGIN_REDIRECT_URL = '/'
+	# Database
+	# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+	from .db_password import DBPASS
+
+	DATABASES = {
+	    'default': {
+	        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	        'NAME': "mvpland",
+	        'USER': "cfedeploy",
+	        'PASSWORD': DBPASS,
+	    }
+	}
+
+
+	# Internationalization
+	# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+	LANGUAGE_CODE = 'en-us'
+
+	TIME_ZONE = 'UTC'
+
+	USE_I18N = True
+
+	USE_L10N = True
+
+	USE_TZ = True
+
+
+	# Static files (CSS, JavaScript, Images)
+	# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+	STATIC_URL = '/static/'
+
+	STATIC_ROOT = '/home/cfedeploy/webapps/mvpland_static/'
+	#os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_root")
+	    
+	STATICFILES_DIRS = (
+	    os.path.join(BASE_DIR, "static_in_pro", "our_static"),
+	    #os.path.join(BASE_DIR, "static_in_env"),
+	    #'/var/www/static/',
+	)
+
+	MEDIA_URL = '/media/'
+	MEDIA_ROOT = '/home/cfedeploy/webapps/mvpland_media/'
+	#os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "media_root")
+
+
+	#Crispy FORM TAGs SETTINGS
+	CRISPY_TEMPLATE_PACK = 'bootstrap3'
+
+
+	#DJANGO REGISTRATION REDUX SETTINGS
+	ACCOUNT_ACTIVATION_DAYS = 7
+	REGISTRATION_AUTO_LOGIN = True
+	SITE_ID = 1
+	LOGIN_REDIRECT_URL = '/'
+
+
+
+
+
