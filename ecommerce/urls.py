6136490cac5ae0bd265f7d6735d87578aa85d355
Maelstroms38 <stroms38@gmail.com>
2016-02-05 12:38:07 -0500

    static files

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -43,5 +43,4 @@ if settings.DEBUG:
 if not settings.DEBUG:
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
